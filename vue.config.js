f353e0ff3bd063e2b8915784d92970d827dbc697
lcglcglcg <1421061623@qq.com>
2022-02-19 16:43:19 +0800

    增加dist

--- vue.config.js
+++ vue.config.js
@@ -8,12 +8,13 @@ function resolve(dir) {
 module.exports = {
   lintOnSave: false,
   outputDir: 'dist',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : './',
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   productionSourceMap: false,
 
   // 打包app时放开该配置
   // publicPath:'./',
-  configureWebpack: (config) => {
+  configureWebpack: config => {
     // 生产环境取消 console.log
     if (process.env.NODE_ENV === 'production') {
       config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
@@ -23,10 +24,8 @@ module.exports = {
       config.devtool = 'source-map'
     }
   },
-  chainWebpack: (config) => {
-    config.resolve.alias
-      .set('@$', resolve('src'))
-      .set('@api', resolve('src/api'))
+  chainWebpack: config => {
+    config.resolve.alias.set('@$', resolve('src')).set('@api', resolve('src/api'))
 
     // 生产环境，开启js\css压缩
     if (process.env.NODE_ENV === 'production') {
@@ -34,7 +33,7 @@ module.exports = {
         new CompressionPlugin({
           test: /\.js$|\.css|\.less/, // 匹配文件名
           threshold: 10240, // 对超过10k的数据压缩
-          deleteOriginalAssets: true, // 不删除源文件
+          deleteOriginalAssets: true // 不删除源文件
         })
       )
     }
@@ -55,11 +54,11 @@ module.exports = {
           /* less 变量覆盖，用于自定义 ant design 主题 */
           'primary-color': '#1890FF',
           'link-color': '#1890FF',
-          'border-radius-base': '4px',
+          'border-radius-base': '4px'
         },
-        javascriptEnabled: true,
-      },
-    },
+        javascriptEnabled: true
+      }
+    }
   },
 
   devServer: {
@@ -68,8 +67,8 @@ module.exports = {
       '/jeecg-boot': {
         target: 'http://localhost:8080', // 请求本地 需要jeecg-boot后台项目
         ws: false,
-        changeOrigin: true,
-      },
-    },
-  },
+        changeOrigin: true
+      }
+    }
+  }
 }
